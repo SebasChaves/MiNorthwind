9d163f6a0cfece15551e061b8e93ed8ad4304bff
sebas <sebas.chacon.e@gmail.com>
2023-02-06 20:18:09 -0600

    Shipper : Details e Index

---
+++ MiNorthwind/FrontEnd/Controllers/ShipperController.cs
@@ -0,0 +1,93 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class ShipperController : Controller
+    {
+        // GET: ShipperController
+
+        ShipperHelper shipperHelper;
+        public ActionResult Index()
+        {
+
+            shipperHelper = new ShipperHelper();
+            List<ShipperViewModel> lista = shipperHelper.GetAll();    
+
+            return View(lista);
+        }
+
+        // GET: ShipperController/Details/5
+        public ActionResult Details(int id)
+        {
+            shipperHelper = new ShipperHelper();
+            ShipperViewModel shipperGet = shipperHelper.Get(id);
+            return View(shipperGet);
+        }
+
+        // GET: ShipperController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShipperController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShipperController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ShipperController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShipperController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ShipperController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MiNorthwind/FrontEnd/Helpers/ShipperHelper.cs
@@ -0,0 +1,33 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FrontEnd.Helpers
+{
+    public class ShipperHelper
+    {
+        private ServiceRepository serviceRepository;
+
+        public ShipperHelper()
+        {
+            serviceRepository= new ServiceRepository();
+        }
+
+        public List<ShipperViewModel> GetAll()
+        {
+            HttpResponseMessage response = serviceRepository.GetResponse("api/Shipper");
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            List <ShipperViewModel> lista = JsonConvert.DeserializeObject<List<ShipperViewModel>>(content);
+            return lista;
+        }
+
+        public ShipperViewModel Get(int id) {
+            HttpResponseMessage response = serviceRepository.GetResponse("api/Shipper/"+id.ToString());
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            ShipperViewModel shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+            return shipper;
+        }
+    }
+}
